d234a0a760055ebc51954fcd12bf5c677ad3143b
Eileen <eileen.tallman@gmail.com>
2018-11-20 13:58:10 -0500

    Completed submission

--- tower.js
+++ tower.js
@@ -1,21 +1,54 @@
-const gameBoardDiv = 
+let tower = document.getElementsByClassName("tower")
+let towerLeftDiv = document.getElementById("towerLeft");
+let towerMiddleDiv = document.getElementById("towerMiddle");
+let towerRightDiv = document.getElementById("towerRight");
+
+let tower1
+let tower2
+let currentDisc
+let selected = false;
 
 
-function selection() {
+//Tower Click Functions//
+towerLeftDiv.onclick = function () {
+    clicked(event);
+}
 
-    towerLeftDiv.addEventListener('click', function () {
-        console.log("Are you sure you can defeat me with Roncor?")
-        gamePlay("Roncor")
-    });
+towerMiddleDiv.onclick = function () {
+    clicked(event);
+}
 
-    towerMiddleDiv.addEventListener('click', function () {
-        console.log("Are you sure you can defeat me with Printlor?")
-        gamePlay("Printlor")
-    });
+towerRightDiv.onclick = function () {
+    clicked(event);
+    const count = towerRight.childElementCount;
+    if (count == 4) {
+        alert("YOU WIN!!")
+    }
+}
 
-    towerRightDiv.addEventListener('click', function () {
-        console.log("Are you sure you can defeat me with Stan?")
-        gamePlay("Stan")
-    });
 
+function clicked(event) {
+
+    if (selected) {
+        tower2 = event.currentTarget;
+        moveDisc()
+
+    } else {
+        tower1 = event.currentTarget;
+        currentDisc = tower1.lastElementChild
+        tower1.removeChild(tower1.lastElementChild)
+    }
+    selected = !selected
+}
+
+function moveDisc() {
+    if (tower2.childElementCount > 0) {
+        if (currentDisc.id.slice(1) < tower2.lastElementChild.id.slice(1)) {
+            tower2.appendChild(currentDisc)
+        } else {
+            tower1.appendChild(currentDisc)
+        }
+    } else {
+        tower2.appendChild(currentDisc)
+    }
 }
